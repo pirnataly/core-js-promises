a94b487c75b0a7e4a4544e3b679f9b2947e22d9b
pirnataly <piroznatal@gmail.com>
2024-04-08 22:53:31 +0300

    feat: add 1 task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,15 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  const promise = new Promise(function abc(resolve, reject) {
+    if (number >= 0) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+  return promise;
 }
 
 /**
@@ -36,7 +43,6 @@ function getPromise(/* number */) {
 function getPromiseResult(/* source */) {
   throw new Error('Not implemented');
 }
-
 /**
  * Takes an array of promises and returns a promise that resolves with the value of the first successfully resolved promise From the given array.
  * If all promises in the array are rejected, it returns a promise that is rejected.
